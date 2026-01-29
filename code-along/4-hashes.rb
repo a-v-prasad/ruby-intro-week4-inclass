9f61315dac7f4602a8153dbc1f39a1a3117bf273
a-v-prasad <alisha.prasad@kellogg.northwestern.edu>
2026-01-29 02:31:23 +0000

    4-hashes in class code-along. review last section with timeline hash

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -1,9 +1,30 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 4-hashes.rb
-
+#puts"hi"
 # Hashes are lists of *key-value pairs*
+profile = {"name" =>"ben", "location"=>"chicago", "status"=>"Teaching"}
+
+puts profile
 
 # Accessing data from the hash
 
+puts profile ["name"]
+#puts "hello, #{name}"
+
+profile ["age"] = 44
+profile["name"] = "benjamin"
+
+puts profile
+
+
+#need to use the key
+
 # More Complex Hashes
+
+profile["location"] = {"city"=> "chicago", "state" =>"IL"}
+puts profile
+
+puts profile ["location"]["state"]
+
+#timeline =
