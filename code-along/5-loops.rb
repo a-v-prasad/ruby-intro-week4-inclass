cd76628c4262b896cb690cf8920be3fc759c2d61
a-v-prasad <alisha.prasad@kellogg.northwestern.edu>
2026-01-29 03:15:43 +0000

    code for loops added in lecture - review

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -1,11 +1,29 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
-
+#puts "hi"
 # Infinite Tacos -> Finite Tacos
 # loop do
 #   puts "tacos!"
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado","avocado", "sweet Potato"]
+
+# index = 0
+
+# loop do
+# if index == tacos.length
+#   break
+# end
+# #.size, .count and .length are aliases of each other
+# flavor = tacos[index]
+# puts"#{flavor} tacos"
+
+# index =index+1
+# end
+
+for taco in tacos
+  puts "#{taco} tacos"
+end
+  
